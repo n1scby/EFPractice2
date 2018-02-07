c1e9add61ae2de6335e35f75f51863fffa922780
Nic B <n1scby@yahoo.com>
2018-02-06 19:35:37 -0500

    EF from console

---
+++ src/EFPractice2/EFPractice2/BloggingContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFPractice2
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFPractice2;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ src/EFPractice2/EFPractice2/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EFPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Blog myBlog = new Blog
+            //{
+            //    Url = "ccalearn.tech",
+            //    Rating = 4
+            //};
+
+            using (var db = new BloggingContext())
+            {
+                var myBlog = db.Blogs.Include(b => b.Posts).FirstOrDefault(b => b.BlogId == 1);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    var myPost = new Post
+                    {
+                        Blog = myBlog,
+                        Title = $"Post {i}",
+                        BlogId = myBlog.BlogId,
+                        Content = $"<h1>The Second Post!</h1><p>This is post {i}</p>"
+                    };
+
+                    // db.Blogs.Add(myBlog);
+                    db.Posts.Add(myPost);
+                }
+                db.SaveChanges();
+            }
+
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
